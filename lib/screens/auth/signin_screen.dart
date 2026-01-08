fd6148e3d00731bd9fba497290a4ff0e42e0de76
unknown <reyesjohnkarlo7@gmail.com>
2026-01-08 10:21:32 +0800

    add assets

---
+++ lib/screens/auth/signin_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SigninScreen extends StatefulWidget {
+  const SigninScreen({super.key});
+
+  @override
+  State<SigninScreen> createState() => _SigninScreenState();
+}
+
+class _SigninScreenState extends State<SigninScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: 
+      Center(
+        child: Icon(Icons.add_business_outlined)
+        
+    )
+   );
+  }
+}
