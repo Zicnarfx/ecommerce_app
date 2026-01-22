f6e0217c6832236f8a15f7d0481bea66db22de1b
unknown <reyesjohnkarlo7@gmail.com>
2026-01-22 10:13:48 +0800

    homescreen

--- lib/screens/auth/signin_screen.dart
+++ lib/screens/auth/signin_screen.dart
@@ -1,4 +1,7 @@
+import 'package:ecommerce_app/screens/auth/signup_screen.dart';
+import 'package:ecommerce_app/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
 
 class SigninScreen extends StatefulWidget {
   const SigninScreen({super.key});
@@ -8,6 +11,39 @@ class SigninScreen extends StatefulWidget {
 }
 
 class _SigninScreenState extends State<SigninScreen> {
+  final TextEditingController  _usernameController = TextEditingController();
+  final TextEditingController  _passwordController = TextEditingController();
+  String username = "admin";
+  String password = "password";
+
+  void signin(){
+    if (
+      _usernameController.text == username && _passwordController.text == password
+    ){
+      Navigator.push(context, MaterialPageRoute(builder: (_) => HomeScreen()));
+    }
+
+    else{
+                const snackBar = SnackBar(
+                  /// need to set following properties for best effect of awesome_snackbar_content
+                  elevation: 0,
+                  behavior: SnackBarBehavior.floating,
+                  backgroundColor: Colors.transparent,
+                  content: AwesomeSnackbarContent(
+                    title: 'On Snap!',
+                    message:
+                        'User or password is incorrect',
+
+                    /// change contentType to ContentType.success, ContentType.warning or ContentType.help for variants
+                    contentType: ContentType.failure,
+                  ),
+                );
+
+                ScaffoldMessenger.of(context)
+                  ..hideCurrentSnackBar()
+                  ..showSnackBar(snackBar);
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,20 +54,20 @@ class _SigninScreenState extends State<SigninScreen> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              // logo
               Image.asset(
-                'assets/images/cartlogo.png',
-                height: 150,
-                width: 160,
+                'assets/images/ecommerceapplogo.png',
+                scale: 1,
               ),
 
 
               SizedBox(
                 height: 10,
               ),
-              // LOGIN
+              // text
               Text(
                   style: TextStyle(
-                      fontSize: 15,
+                      fontSize: 15.9,
                       color: Colors.black),
                   "Welcome Back! Please Login!"),
 
@@ -39,16 +75,17 @@ class _SigninScreenState extends State<SigninScreen> {
               SizedBox(
                 height: 30,
               ),
-              // email textfield
+              // user textfield
               SizedBox(
                 width: double.infinity,
                 child: TextField(
+                  controller: _usernameController,
                 decoration: InputDecoration(
                   prefixIcon: Icon(
                     color: const Color.fromARGB(255, 35, 0, 49),
                     Icons.person),
                   label: Text(
-                    style: TextStyle(color: const Color.fromARGB(255, 105, 5, 5)),"Username"),
+                    style: TextStyle(color: const Color.fromARGB(255, 24, 0, 37)),"Username"),
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(10),
                   ),
@@ -64,10 +101,12 @@ class _SigninScreenState extends State<SigninScreen> {
                SizedBox(
                 width: double.infinity,
                  child: TextField(
-                    
+                  controller: _passwordController,
+                  obscureText: true,
                                decoration: InputDecoration(
                   prefixIcon: Icon(Icons.lock_open_outlined),
-                  label: Text("Password"),
+                  label: Text(
+                    style: TextStyle(color: const Color.fromARGB(255, 24, 0, 37)),"Password"),
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(10),
                   ),
@@ -85,10 +124,12 @@ class _SigninScreenState extends State<SigninScreen> {
               height: 50,
               width: double.infinity,
               child: ElevatedButton(
-                style: ElevatedButton.styleFrom(backgroundColor: const Color.fromARGB(255, 105, 5, 5)),
+                style: ElevatedButton.styleFrom(backgroundColor: const Color.fromARGB(255, 190, 0, 133)),
 
-                onPressed: (){},
-                child: Text(style: TextStyle(color: const Color.fromARGB(255, 255, 255, 255)),
+                onPressed: signin,
+                child: Text(style: TextStyle(fontSize: 16.0,
+                letterSpacing: 3,
+                  color: const Color.fromARGB(255, 0, 0, 0)),
                   "Login"),
               ),
             ),
@@ -98,11 +139,17 @@ class _SigninScreenState extends State<SigninScreen> {
                 height: 30,
               ),
               // register
-              Text(
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black),
-                  "Register"),
+             TextButton(
+  onPressed: () {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const SignupScreen()),
+    );
+  },
+  child: const Text(
+    style: TextStyle(fontSize: 20),
+    'Sign Up'),
+)
             ],
           ),
         ),
